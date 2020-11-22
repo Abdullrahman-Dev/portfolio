186d4180287443542f62f47fc9ebe84920c5ac62
unknown <you@example.com>
2020-11-22 13:43:23 +0200

    Fix Responsive Nav & Header

--- js/file.js
+++ js/file.js
@@ -1,33 +1,33 @@
 /*==================================================================
 ============== NavBar =============================================
 =================================================================*/
+
+/*===================== Add sticky Class =================*/
 var nav           = document.querySelector("#navbar");
 var iconContainer = nav.querySelector("span.icon");
-var blueIcon      = '<img src="images/icons/blue-icon.png"  alt="icon">';
+var greenIcon     = '<img src="images/icons/green-icon.png"  alt="icon">';
 var whiteIcon     = '<img src="images/icons/white-icon.png"  alt="icon">';
-/*========== Check If in Mideum Page ============*/
+/*=== Check If in Mideum Page ===*/
 if( window.scrollY >  50 ){
     nav.classList.add("navbar-sticky");
-    iconContainer.innerHTML = blueIcon;
+    iconContainer.innerHTML = greenIcon;
 }else{
     nav.classList.remove("navbar-sticky");
     iconContainer.innerHTML = whiteIcon;
 }
-/*==========  OnScroll ============*/
+/*==  OnScroll ==*/
 window.onscroll = function (e) {  
     if( this.scrollY >  50 ){
         nav.classList.add("navbar-sticky");
-        iconContainer.innerHTML = blueIcon;
+        iconContainer.innerHTML = greenIcon;
     }else{
         nav.classList.remove("navbar-sticky");
         iconContainer.innerHTML = whiteIcon;
     }
-} 
+}
 
-
-/*=====  Mobile Nav ======*/
+/*====== at click Nav Bar ======*/
 var bar      = nav.querySelector(".bar");
-var barLines = bar.querySelector("div");
 var navLinks = nav.querySelector(".nav-links");
 var links    = nav.querySelectorAll(".nav-links li");
 bar.addEventListener('click', ()=>{
